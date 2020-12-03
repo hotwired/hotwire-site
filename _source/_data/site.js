bdb1a4c641fe06127e679f1fc2500a55736972ff
Adam Stoddard <adam@adamstoddard.com>
2020-12-02 16:25:56 -0800

    rough in  landing page

---
+++ _source/_data/site.js
@@ -0,0 +1,13 @@
+// bits of text for use site-wide
+const name = 'Hotwire';
+const title = 'Hotwire';
+const description = 'HTML Over The Wire';
+const date = new Date();
+const currentYear = date.getFullYear();
+
+module.exports = {
+  name,
+  title,
+  description,
+  currentYear,
+};
